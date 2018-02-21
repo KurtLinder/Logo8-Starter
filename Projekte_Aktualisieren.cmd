99d7cb583e5655da8b8ee09f2607462dea46e82f
Kurt Linder <32621408+KurtLinder@users.noreply.github.com>
2018-02-21 11:53:39 +0100

    Projekte löschen/aktualisieren

---
+++ Projekte_Aktualisieren.cmd
@@ -0,0 +1,3 @@
+rd "c:\git\Logo8-Starter\Logo8_Starter\bin\Debug\Projekte\" /S /Q
+xcopy "s:\Linder Kurt\Logo8\Projekte\*.*"  "c:\git\Logo8-Starter\Logo8_Starter\bin\Debug\Projekte\" /k/h/e/v/c/q/r 
+pause
---
+++ Projekte_Loeschen.cmd
@@ -0,0 +1,2 @@
+rd "c:\git\Logo8-Starter\Logo8_Starter\bin\Debug\Projekte\" /S /Q
+pause
